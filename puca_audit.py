93dd8a7663ac074361a58a31f69367590f324977
Avery Garon <averygaron@gmail.com>
2021-12-02 04:41:41 +0000

    Incorporated EddyViscosity's full export function

--- puca_audit.py
+++ puca_audit.py
@@ -1,11 +1,14 @@
 """
 Download the executable version at https://drive.google.com/file/d/1TGGFf8OWZV6t-aCFm0c54Uw_SAXeO9N3/view?usp=sharing
 
+For the smaller version that excludes the plotting option, see 
+
 This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License. Original author is dude1818 on PucaTrade.com (https://pucatrade.com/profiles/show/129317)
 """
 
 from requests import Session
 from pprint import pformat
+from re import findall
 import matplotlib.pyplot as plt
 
 # Global variables
@@ -19,10 +22,11 @@ def get_query():
     print("Choose what data to collect. Options:")
     print("    (T)ransactions (sends/receives, fees, etc.)")
     print("    (D)istribution of card values sent/received")
+    print("    (E)xport entire ledger")
 
     query = input("").upper()
-    while query not in ("T", "D"):
-        print("Please enter T or D")
+    while query not in ("T", "D", "E"):
+        print("Please enter T, D, or E")
         query = input("").upper()
 
     return query
@@ -78,7 +82,7 @@ def get_transactions(urls, payload):
 
             for transaction in trans_list:
                 type = transaction.split('"value letter">')[1].split("</div>")[0]
-                points = int(
+                points = (
                     transaction.split("POINTS")[1]
                     .split('"value">')[1]
                     .split("</div>")[0]
@@ -209,16 +213,32 @@ def get_values(urls, payload):
 
 
 def make_histogram(sending, receiving):
-    """Plot a histogram of card values. Require matplotlib"""
+    """Plot a histogram of card values. Requires matplotlib"""
 
-    n, bins, patches = plt.hist(sending+receiving, bins=25, log=True)
+    n, bins, patches = plt.hist(sending + receiving, bins=25, log=True)
     plt.clf()
 
     # Transparent histograms with solid edges
-    plt.hist(sending, bins=bins, log=True, edgecolor="None", alpha=0.5, color="b", label="Sent")
+    plt.hist(
+        sending,
+        bins=bins,
+        log=True,
+        edgecolor="None",
+        alpha=0.5,
+        color="b",
+        label="Sent",
+    )
     plt.hist(sending, bins=bins, log=True, lw=1, facecolor="None", edgecolor="k")
 
-    plt.hist(receiving, bins=bins, log=True, edgecolor="None", alpha=0.5, color="r", label="Received")
+    plt.hist(
+        receiving,
+        bins=bins,
+        log=True,
+        edgecolor="None",
+        alpha=0.5,
+        color="r",
+        label="Received",
+    )
     plt.hist(receiving, bins=bins, log=True, lw=1, facecolor="None", edgecolor="k")
 
     plt.legend()
@@ -226,8 +246,167 @@ def make_histogram(sending, receiving):
     plt.ylabel("Count")
     plt.title("Distribution of card values traded")
     plt.tight_layout()
-    
+
     plt.show()
+
+
+def quote_if_has_comma(text):
+    """Enclose a string in quotes if it contains a comma"""
+
+    if "," in text:
+        return '"%s"' % text
+    else:
+        return text
+
+
+def get_name_and_id(s):
+    """Extract user ID and user name from an href"""
+
+    s2 = s.split("/show/")[1]
+    # Now s2 looks like "12345'>John Doe</a>"
+    s3 = s2.split("'>")
+    id = int(s3[0])
+    name = s3[1].split("</a>")[0]
+
+    return quote_if_has_comma(name), id
+
+
+class Transaction:
+    sender_name = ""
+    sender_id = -1
+    receiver_name = ""
+    receiver_id = -1
+    type = ""
+    notes = ""
+    package_id = ""
+    card_id = ""
+    card_name = ""
+    foil = False
+    points = 0
+    running_total = 0
+    date = ""
+    time = ""
+
+    # Create a Transaction object from a transaction text block
+    def __init__(self, ttb):
+        keysplits = ttb.split('<div class="label')
+        valuesplits = ttb.split('<div class="value')
+        for iks, keysplit in enumerate(keysplits):
+            valuesplit = valuesplits[iks]
+            ka = keysplit.find(">") + 1
+            kb = keysplit.find("</div>")
+            key = keysplit[ka:kb].strip()
+            va = valuesplit.find(">") + 1
+            vb = valuesplit.find("</div>")
+            value = valuesplit[va:vb].strip()
+
+            if key == "SENDER":
+                self.sender_name, self.sender_id = get_name_and_id(value)
+            elif key == "TYPE":
+                if value == "WANT":
+                    self.type = "TRADE FEE"
+                else:
+                    self.type = value
+            elif key == "NOTES":
+                self.notes = value
+            elif key == "POINTS":
+                # Gift trades have an icon in place of the point value, which
+                # shows up as:
+                #   <span class="icon icon-gift ">Gift</span>
+                # in the HTML. So before setting a point value, check if the
+                # trade was a gift
+                if "Gift" in value:
+                    self.type = "GIFT"
+                    self.points = 0
+                else:
+                    # Not a gift
+                    self.points = int(value.replace(",", ""))
+            elif key == "RUNNING":
+                self.running_total = int(value.split(">")[-1].replace(",", ""))
+            elif key == "RECEIVER":
+                self.receiver_name, self.receiver_id = get_name_and_id(value)
+            elif key == "DATE":
+                s2 = value.split(" ")
+                self.date = s2[0]
+                self.time = s2[1]
+
+        # Decide what to do with the information in the
+        # "notes" field. This depends on the transaction type.
+        if self.type == "TRADE":
+            # Get the card name, card ID, package ID, and foil identifier
+            numbers = findall(r"\d+", self.notes)
+            self.package_id = numbers[0]
+            self.card_id = numbers[2]
+            self.foil_id = numbers[3] == 0
+            self.card_name = quote_if_has_comma(
+                self.notes.split("</a>")[-2].split(">")[-1]
+            )
+
+    # Return a string representation of this Transaction
+    # suitable for a CSV file.
+    def csv_row(self):
+        return "%s,%s,%i,%i,%s,%i,%s,%i,%s,%s,%s,%s,%s\n" % (
+            self.package_id,
+            self.type,
+            self.points,
+            self.running_total,
+            self.sender_name,
+            self.sender_id,
+            self.receiver_name,
+            self.receiver_id,
+            self.card_name,
+            self.card_id,
+            self.foil,
+            self.date,
+            self.time,
+        )
+
+    # Return a string representation of this Transaction
+    # suitable for print statements
+    def __repr__(self):
+        if self.type == "TRADE":
+            return (
+                "Package %d:\n" % self.package_id
+                + "  %s (%i) -> %s (%i)\n"
+                % (
+                    self.sender_name,
+                    self.sender_id,
+                    self.receiver_name,
+                    self.receiver_id,
+                )
+                + "  %s for %i pp on %s at %s\n"
+                % (self.card_name, self.points, self.date, self.time)
+            )
+        elif self.type == "PUCASHIELD":
+            return "PUCASHIELD for %d\n" % self.points
+
+
+def get_all_transactions(urls, payload, csvfilename):
+    """Go through each page of ledger and sum up entries"""
+
+    csv_header = "Package ID,Transaction Type,Points,Balance,Sender,Sender ID,Receiver,Receiver ID,Card name,Card ID,Foil,Date,Time\n"
+    transaction_start_string = '<div class="column sender">'
+
+    with open(csvfilename, "w") as f:
+        f.write(csv_header)
+        with Session() as session:
+            post = session.post(login_url, data=payload)
+            for url in urls:
+                r = session.get(url)
+                transaction_text_blocks = r.text.split(transaction_start_string)[2:]
+                print(
+                    f"    Adding %i transactions from %s"
+                    % (len(transaction_text_blocks), url.split("/")[-1])
+                )
+
+                for ttb in transaction_text_blocks:
+                    # Remove non-ASCII characters to prevent usernames
+                    # with unusual characters from causing a crash later
+                    f.write(
+                        Transaction(
+                            ttb.encode("ascii", "ignore").decode("ascii")
+                        ).csv_row()
+                    )
 
 
 if __name__ == "__main__":
@@ -254,7 +433,7 @@ if __name__ == "__main__":
 
     elif query == "D":
         sending, receiving = get_values(urls, payload)
-        fout = "puca_transaction_values.txt"
+        fout = "puca_histogram.txt"
 
         with open(fout, "w") as f:
             f.write("sending = " + str(sending) + "\n\n")
@@ -262,5 +441,10 @@ if __name__ == "__main__":
             print("Transaction summary written to %s\n" % fout)
 
         make_histogram(sending, receiving)
+
+    elif query == "E":
+        fout = "puca_ledger.csv"
+        get_all_transactions(urls, payload, fout)
+        print("Transaction summary written to %s\n" % fout)
 
     input("Press Enter to quit")
